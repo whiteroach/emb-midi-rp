837c423555d28fdd4398c31746c7ffab39b57bba
psim <p.simcic@tutanota.com>
2025-09-27 20:56:19 +0200

    27092025-it-blinks

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
 #![no_std]
 #![no_main]
 
-
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
-use embassy_time::{Duration, Timer};
+use embassy_time::Timer;
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // Initialise Peripherals
     let p = embassy_rp::init(Default::default());
-
-    // Create LED
     let mut led = Output::new(p.PIN_25, Level::Low);
 
-    // Loop
     loop {
-        // Log
-        info!("LED On!");
-
-        // Turn LED On
+        info!("led on!");
         led.set_high();
+        Timer::after_secs(1).await;
 
-        // Wait 100ms
-        Timer::after(Duration::from_millis(100)).await;
-
-        // Log
-        info!("LED Off!");
-
-        // Turn Led Off
+        info!("led off!");
         led.set_low();
-
-        // Wait 100ms
-        Timer::after(Duration::from_millis(100)).await;
+        Timer::after_secs(1).await;
     }
 }
